8fce06f73cee15437a8cae3c6cbbe7dc1167b3ea
John Ratcliff <john.w.ratcliff@biola.edu>
2018-08-19 22:28:01 +0000

    Update eslint-mammoth

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+/* eslint-env node */
+
 module.exports = {
   root: true,
 
@@ -9,7 +11,6 @@ module.exports = {
   },
 
   extends: [
-    'eslint:recommended',
     'plugin:ember/recommended'
   ],
 
@@ -21,20 +22,22 @@ module.exports = {
     /*
       Ember-ESlint Rules
     **/
-    'ember/new-module-imports': 1,
-    'ember/no-duplicate-dependent-keys': 1,
-    'ember/require-super-in-init': 2,
-    'ember/alias-model-in-controller': 1,
+    'ember/no-empty-attrs': 2,
+    'ember/use-ember-get-and-set': 2, // Has --fix !
+    'ember/new-module-imports': 2,
+    'ember/no-old-shims': 2, // Has --fix !
 
-    // Not now
-    'ember/order-in-components': 0,
-    'ember/order-in-controllers': 0,
-    'ember/order-in-models': 0,
-    'ember/order-in-routes': 0,
+    'ember/no-observers': 1,
 
-    /*
-      ESLint Rules
-    **/
+    // Eventually
+    'ember/order-in-components': 0, // Has --fix !
+    'ember/order-in-models': 0, // Has --fix !
+    'ember/order-in-routes': 0, // Has --fix !
+    'ember/order-in-controllers': 0, // Has --fix !
+
+    // /*
+    //   ESLint Rules
+    // **/
     'no-confusing-arrow': 2,
     'no-const-assign': 2,
     'no-dupe-class-members': 2,
@@ -65,24 +68,22 @@ module.exports = {
     'newline-after-var': 1,
     'no-var': 1,
     'quotes': [1, 'single', 'avoid-escape'],
+    'no-unused-vars': 1,
 
-    // TODO
-    'curly': 0,
+    // // TODO
+    // 'curly': 0,
 
-    // Important, but currently has compatibility issues
-    'no-unused-vars': 0,
+    // 'strict': 0,
+    // 'no-else-return': 0,
+    // 'no-ternary': 0,
+    // 'comma-dangle': 0,
+    // 'no-underscore-dangle': 0,
 
-    'strict': 0,
-    'no-else-return': 0,
-    'no-ternary': 0,
-    'comma-dangle': 0,
-    'no-underscore-dangle': 0,
-
-    // Possibly consider
-    'no-magic-numbers': 0,
-    'consistent-return': 0,
-    'prefer-arrow-callback': 0,
-    'valid-jsdoc': 0,
-    'no-console': 0
+    // // Possibly consider
+    // 'no-magic-numbers': 0,
+    // 'consistent-return': 0,
+    // 'prefer-arrow-callback': 0,
+    // 'valid-jsdoc': 0,
+    // 'no-console': 0
   }
 };
